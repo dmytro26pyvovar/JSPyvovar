b1767051db7defd97fb712976a197bbce937830b
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-03 14:36:31 +0200

    Create task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryImages = document.querySelector('.gallery');
+console.dir(galleryImages);
+images.forEach((image) => {
+   const Item = document.createElement('li');
+      Item.classList.add('gallery-items');
+
+      const img = document.createElement('img');
+      img.src = image.url;
+      img.alt = image.alt;
+
+      Item.appendChild(img);
+      galleryImages.insertAdjacentElement('beforeend', Item);
+});
