44198f7550993580f8ccb6c2453971e30e7ca6e4
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-04 20:59:13 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const Input = document.querySelector('#validation-input');
 Input.addEventListener('blur', function () {
     const inputValue = Input.value;
-    const validLength = Input.getAttribute('data-length');
+    const validLength = Input.getAttribute('[data-length]');
     if (inputValue === validLength) {
         Input.classList.remove('invalid');
         Input.classList.add('valid')
