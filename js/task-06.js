46bd266253ba1184166d4cfa2ebec82f1a96036e
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-06 20:24:01 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,15 @@
 const Input = document.querySelector('#validation-input');
-Input.addEventListener('blur', function () {
-    const inputValue = Input.value;
-    const validLength = Input.getAttribute('data-length');
-    if (inputValue === validLength) {
+
+    function onValidationInput (event) {
+    
+    if (event.currentTarget.value.length === Number(Input.dataset.length)) {
         Input.classList.remove('invalid');
         Input.classList.add('valid')
     } else {
         Input.classList.remove('valid');
         Input.classList.add('invalid');
     }
-});
+};
+
+Input.addEventListener('blur', onValidationInput)
+
