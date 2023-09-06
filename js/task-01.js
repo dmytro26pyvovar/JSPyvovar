c6a0a942689491ac6f02fda411a4feb16e8ee036
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-06 20:23:56 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,23 @@
-const list = document.querySelector('#categories')
-console.log(list)
-const items = document.querySelectorAll('li.item')
-console.log(items)
-items.forEach((item) => {
-    const Name = item.querySelector('h2').textContent;
-    const Elements = item.querySelectorAll('ul > li');
-    console.log(`Category: ${Name}`);
+// const list = document.querySelector('#categories')
+// console.log(list)
+// const items = document.querySelectorAll('li.item')
+// console.log(items)
+// items.forEach((item) => {
+//     const Name = item.querySelector('h2').textContent;
+//     const Elements = item.querySelectorAll('ul > li');
+//     console.log(`Category: ${Name}`);
+//   console.log(`Elements: ${Elements.length}`);
+// })
+
+const list = document.querySelector('#categories');
+const Items = list.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${Items.length}`);
+
+Items.forEach((Item) => {
+  const Name = Item.firstElementChild.textContent;
+  const Elements = Item.lastElementChild.querySelectorAll('li');
+
+  console.log(`Category: ${Name}`);
   console.log(`Elements: ${Elements.length}`);
-})
+});
