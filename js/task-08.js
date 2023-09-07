d3c906989c51c84aa3ad3a14157861a8b3f3b0eb
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-07 12:50:52 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,7 +2,18 @@ const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', function (event) {
     event.preventDefault();
- 
     const userData = {};
+    const formData = new FormData(loginForm);
+
+     formData.forEach(function(value, key) {
+                userData[key] = value;
+     });
+    if (Object.values(userData).every(value => value)) {
+        console.log(userData);
+        
+    } else {
+            alert('Всі поля повинні бути заповнені.');
+        }
+loginForm.reset();
 
 })
