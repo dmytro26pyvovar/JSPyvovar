c99453aaf5a7e73af12e928200bd18bed56cef9c
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-11 12:33:52 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(link => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${link.largeImage}">
+      <img
+        class="gallery__image"
+        src="${link.smallImage}"
+        data-source="${link.largeImage}"
+        alt="${link.imageDescription}"
+        lodaing = "lazy"
+      />
+    </a>
+  </li>
+`).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
 
 console.log(galleryItems);
